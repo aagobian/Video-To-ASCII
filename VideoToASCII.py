2570e200d78e6799be1678d05fa6549a09bc380b
Arees Agobian <areesagob@gmail.com>
2024-04-14 22:38:26 -0400

    Add image pixelization

--- VideoToASCII.py
+++ VideoToASCII.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 
-# sets up webcam
+
+# function to pixelate an input image by resizing down and back up
+def pixelate_img(image):
+    width, height, _ = image.shape
+    image = cv.resize(image, (32, 32), interpolation=cv.INTER_AREA)
+    image = cv.resize(image, (height, width), interpolation=cv.INTER_AREA)
+    return image
+
+
+# set up webcam
 cam = cv.VideoCapture(0)
 
 # loop to capture video from the camera and display it on the screen
@@ -9,6 +18,16 @@ while True:
 
     # flip image
     img = cv.flip(img, 1)
+    img = pixelate_img(img)
 
+    # display image on screen
     cv.imshow("Video", img)
-    cv.waitKey(15)
+
+    # press esc key to exit
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+# exit webcam
+cam.release()
+cv.destroyAllWindows()
