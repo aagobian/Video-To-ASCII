39e24a885d83e7b0c32a8c46e591e0800116ab87
Arees Agobian <areesagob@gmail.com>
2024-04-14 23:52:01 -0400

    Add image to ascii character conversion

--- VideoToASCII.py
+++ VideoToASCII.py
@@ -1,13 +1,28 @@
 import cv2 as cv
+import numpy
+
+
+def convert_to_ascii(image):
+    pixels = image.flatten().tolist()
+    ascii_list = []
+    for pixel in pixels:
+        ascii_rep = round(pixel / 4)
+        if ascii_rep == 64:
+            ascii_rep -= 1
+        ascii_list.append(ascii_chars[ascii_rep])
+    return ascii_list
 
 
 # function to pixelate an input image by resizing down and back up
-def pixelate_img(image):
-    width, height, _ = image.shape
+def pixelate_img(image, h, w):
     image = cv.resize(image, (32, 32), interpolation=cv.INTER_AREA)
-    image = cv.resize(image, (height, width), interpolation=cv.INTER_AREA)
+    convert_to_ascii(image)
+    image = cv.resize(image, (h, w), interpolation=cv.INTER_AREA)
     return image
 
+
+# ASCII characters to be used for image output
+ascii_chars = "$@B%8&WM#*oahkbdpqwmZLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,^`'."
 
 # set up webcam
 cam = cv.VideoCapture(0)
@@ -16,9 +31,12 @@ cam = cv.VideoCapture(0)
 while True:
     success, img = cam.read()
 
-    # flip image
+    width, height, _ = img.shape
+
+    # image preprocessing
     img = cv.flip(img, 1)
-    img = pixelate_img(img)
+    img = pixelate_img(img, height, width)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # display image on screen
     cv.imshow("Video", img)
